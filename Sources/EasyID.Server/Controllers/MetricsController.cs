4d12da691e870310f2772da231c0356b67406f08
Vadim Belov <git@belov.us>
2025-09-09 14:31:19 -0700

    Add MetricsController and routing constants
    Introduced the `MetricsController` with a `HasUsers` endpoint to check for user existence in the database. Added a new `Routes` class to define API routing constants, including base, versioned, and metrics-specific routes.

---
+++ Sources/EasyID.Server/Controllers/MetricsController.cs
@@ -0,0 +1,15 @@
+﻿using EasyID.Server.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyID.Server.Controllers
+{
+    public class MetricsController(AppDbContext _dbContext) : ControllerBase
+    {
+        [HttpGet(Routes.Metrics + "/has-users")]
+        public Task<bool> HasUsers()
+        {
+            return _dbContext.Users.AnyAsync();
+        }
+    }
+}
---
+++ Sources/EasyID.Server/Controllers/Routes.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyID.Server.Controllers
+{
+    public static class Routes
+    {
+        public const string Base = "/api";
+
+        public const string Version = Base + "/v1";
+
+        public const string Metrics = Version + "/metrics";
+    }
+}

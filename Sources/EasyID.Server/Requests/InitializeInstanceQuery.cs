23a37257f142e94d938e1856f4e1c57ce7779e1d
Vadim Belov <git@belov.us>
2025-09-15 08:35:34 -0700

    Implement user creation in InitializeInstanceQuery
    Updated `InitializeInstanceQuery.cs` to add necessary using directives and modify the `IInitializeInstanceQueryHandler` class. The `Handle` method now validates login request properties, hashes the password, and creates a new user in the database.
    
    Additionally, the `appsettings.Development.json` file has been updated to include a `PasswordVersion` key in the `Encryption` section, set to `1`, to support the new functionality.

--- Sources/EasyID.Server/Requests/InitializeInstanceQuery.cs
+++ Sources/EasyID.Server/Requests/InitializeInstanceQuery.cs
@@ -1,5 +1,7 @@
-﻿using EasyID.Server.Models.Dto;
-using MediatR;
+﻿using MediatR;
+using EasyID.Server.Database;
+using EasyExtensions.Services;
+using EasyID.Server.Models.Dto;
 
 namespace EasyID.Server.Requests
 {
@@ -8,11 +10,36 @@ namespace EasyID.Server.Requests
         public LoginRequestDto FirstLoginRequest { get; set; } = null!;
     }
 
-    public class IInitializeInstanceQueryHandler : IRequestHandler<InitializeInstanceQuery>
+    public class IInitializeInstanceQueryHandler(IConfiguration _configuration, AppDbContext _dbContext) : IRequestHandler<InitializeInstanceQuery>
     {
-        public Task Handle(InitializeInstanceQuery request, CancellationToken cancellationToken)
+        private readonly string _pepper = _configuration.GetValue<string>("Encryption:Pepper")
+            ?? throw new ArgumentNullException("Encryption:Pepper", "Encryption key cannot be null.");
+        private readonly int _passwordVersion = _configuration.GetValue<int>("Encryption:PasswordVersion", 1);
+
+        public async Task Handle(InitializeInstanceQuery request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            ArgumentException.ThrowIfNullOrWhiteSpace(request.FirstLoginRequest.Username, nameof(request.FirstLoginRequest.Username));
+            ArgumentException.ThrowIfNullOrWhiteSpace(request.FirstLoginRequest.Password, nameof(request.FirstLoginRequest.Password));
+            Pbkdf2PasswordHashService pbk = new(_pepper);
+            string hashedPassword = pbk.Hash(request.FirstLoginRequest.Password);
+            string email = request.FirstLoginRequest.Username.Contains('@') ? request.FirstLoginRequest.Username : "admin@localhost";
+            string username = request.FirstLoginRequest.Username.Contains('@') ? "admin" : request.FirstLoginRequest.Username;
+            User user = new()
+            {
+                Email = email,
+                FailedCount = 0,
+                FirstName = "Admin",
+                LastName = null,
+                ForceReset = false,
+                LockoutUntil = null,
+                MiddleName = null,
+                PasswordPhc = hashedPassword,
+                PasswordVersion = _passwordVersion,
+                PhoneNumber = null,
+                Username = username
+            };
+            await _dbContext.Users.AddAsync(user, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
         }
     }
 }

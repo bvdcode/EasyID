d1a84ef52338c28aff59ee94e0f8937f3ebfa9cb
Vadim Belov <git@belov.us>
2025-09-15 15:07:11 -0700

    Add RefreshToken model and migration, update UserDto with Email property

---
+++ Sources/EasyID.Server/Migrations/20250915220625_t2.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Net;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyID.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class t2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "force_reset",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "lockout_until",
+                table: "users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "password_version",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password_phc",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "refresh_tokens",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    token = table.Column<string>(type: "text", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ip_address = table.Column<IPAddress>(type: "inet", nullable: false),
+                    country = table.Column<string>(type: "text", nullable: false),
+                    city = table.Column<string>(type: "text", nullable: false),
+                    user_agent = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_refresh_tokens", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_refresh_tokens_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_refresh_tokens_token",
+                table: "refresh_tokens",
+                column: "token",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_refresh_tokens_user_id",
+                table: "refresh_tokens",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "refresh_tokens");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "password_version",
+                table: "users",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password_phc",
+                table: "users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "force_reset",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "lockout_until",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
--- Sources/EasyID.Server/Models/Dto/UserDto.cs
+++ Sources/EasyID.Server/Models/Dto/UserDto.cs
@@ -3,6 +3,7 @@
     public class UserDto
     {
         public required Guid Id { get; init; }
+        public required string Email { get; init; }
         public required string Username { get; init; }
     }
 }
